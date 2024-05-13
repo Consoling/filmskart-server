7d06e778688ff584794a2fa3ac13d569821f4e0c
Consoling <shahidarizfannymagnet@gmail.com>
2024-05-13 17:15:31 +0530

    created upcoming movies collections

--- index.js
+++ index.js
@@ -1,35 +1,43 @@
-const express = require('express');
-const fs = require('fs');
+const express = require("express");
+const fs = require("fs");
 
 const app = express();
 
-const cors = require('cors')
+const cors = require("cors");
 
-app.use(cors())
+app.use(cors());
 
+app.get("/product-data", (req, res) => {
+  try {
+    const Data = fs.readFileSync("./constant.json", "utf-8");
+    const sendData = JSON.parse(Data);
+    res.json(sendData);
+  } catch (error) {
+    res.send(error);
+  }
+});
 
-app.get('/product-data', (req, res) => {
-       try{
-        const  Data = fs.readFileSync('./constant.json', 'utf-8');
-       const sendData = JSON.parse(Data);
-       res.json(sendData)
-       } catch(error){
-        res.send(error)
-       }
-})
+app.get("/banner-data", (req, res) => {
+  try {
+    const Data = fs.readFileSync("./bannerData.json", "utf-8");
+    const sendData = JSON.parse(Data);
+    res.json(sendData);
+  } catch (error) {
+    res.send(error);
+  }
+});
 
+app.get("/movie-data", (req, res) => {
+  try {
+    const Data = fs.readFileSync("./upcomingMovies.json", "utf-8");
 
-app.get('/banner-data', (req, res) => {
-    try{
-        const  Data = fs.readFileSync('./bannerData.json', 'utf-8');
-       const sendData = JSON.parse(Data);
-       res.json(sendData)
-       } catch(error){
-        res.send(error)
-       }
-})
-
+    const sendData = JSON.parse(Data);
+    res.json(sendData);
+  } catch (error) {
+    res.send(error);
+  }
+});
 
 app.listen(3002, () => {
-    console.log('Listening...')
-})
+  console.log("Listening...");
+});
